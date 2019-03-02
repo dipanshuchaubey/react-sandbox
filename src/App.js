249b7618b1af09c1aa1369dea9a0f64bbb490699
Dipanshu <dipanshuchaubey@gmail.com>
2019-03-03 03:43:07 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,65 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import ButtonAppBar from "./components/layout/Header";
+import RecipeReviewCard from "./components/Posts";
+import ResponsiveDialog from "./components/layout/About";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <React.Fragment>
+        <ButtonAppBar />
+        <div className="container mt-3 mb-3">
+          <div className="row">
+            <RecipeReviewCard
+              title="Pizza"
+              image="https://knowbbies.com/storage/courses/June2018/35118898_1675576969163519_65233698458435584_n-medium.jpg"
+              content="This impressive paella is a perfect party dish and a fun meal to
+              cook together with your guests. Add 1 cup of frozen peas along
+              with the mussels, if you like."
+            />
+            <RecipeReviewCard
+              title="Samosa"
+              image="https://www.skymetweather.com/themes/skymet/images/gallery/toplists/Top-Not-to-miss-food-items-in-Monsoon/4.jpg"
+              content="This impressive paella is a perfect party dish and a fun meal to
+              cook together with your guests. Add 1 cup of frozen peas along
+              with the mussels, if you like."
+            />
+            <RecipeReviewCard
+              title="Paella dish"
+              image="https://img.timesnownews.com/story/1523535111-spicy_food.PNG?d=600x450"
+              content="This impressive paella is a perfect party dish and a fun meal to
+              cook together with your guests. Add 1 cup of frozen peas along
+              with the mussels, if you like."
+            />
+            <RecipeReviewCard
+              title="Paella dish"
+              image="https://img.timesnownews.com/story/1523535111-spicy_food.PNG?d=600x450"
+              content="This impressive paella is a perfect party dish and a fun meal to
+              cook together with your guests. Add 1 cup of frozen peas along
+              with the mussels, if you like."
+            />
+            <RecipeReviewCard
+              title="Paella dish"
+              image="https://img.timesnownews.com/story/1523535111-spicy_food.PNG?d=600x450"
+              content="This impressive paella is a perfect party dish and a fun meal to
+              cook together with your guests. Add 1 cup of frozen peas along
+              with the mussels, if you like."
+            />
+            <RecipeReviewCard
+              title="Paella dish"
+              image="https://img.timesnownews.com/story/1523535111-spicy_food.PNG?d=600x450"
+              content="This impressive paella is a perfect party dish and a fun meal to
+              cook together with your guests. Add 1 cup of frozen peas along
+              with the mussels, if you like."
+            />
+          </div>
+          <div className="text-center mt-2">
+            <ResponsiveDialog />
+          </div>
+        </div>
+      </React.Fragment>
     );
   }
 }
