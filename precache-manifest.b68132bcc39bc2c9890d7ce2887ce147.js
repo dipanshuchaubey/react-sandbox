0b4f437ec34ca212238bc22aa94d11809831a484
Dipanshu <dipanshuchaubey@gmail.com>
2019-03-03 03:48:45 +0530

    Updates

---
+++ precache-manifest.b68132bcc39bc2c9890d7ce2887ce147.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a179519bc2ceadfd3cb0",
+    "url": "/react-sandbox/static/js/runtime~main.a179519b.js"
+  },
+  {
+    "revision": "b0e60ef9bdde837a4079",
+    "url": "/react-sandbox/static/js/main.b0e60ef9.chunk.js"
+  },
+  {
+    "revision": "8b31d15020417328a0ec",
+    "url": "/react-sandbox/static/js/2.8b31d150.chunk.js"
+  },
+  {
+    "revision": "b0e60ef9bdde837a4079",
+    "url": "/react-sandbox/static/css/main.fc0f4e00.chunk.css"
+  },
+  {
+    "revision": "8b31d15020417328a0ec",
+    "url": "/react-sandbox/static/css/2.b66ed634.chunk.css"
+  },
+  {
+    "revision": "0d6c30c9b00bf5632276c33c82a2c95c",
+    "url": "/react-sandbox/index.html"
+  }
+];
